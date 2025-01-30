653931a1bc9646805ea323a8464c0f09be92bb6e
plus-agc <yamauchi-t@plus-agc.com>
2025-01-30 09:32:53 +0900

    initial commit

---
+++ top.php
@@ -0,0 +1,45 @@
+<?php
+/* Template Name: top */
+?>
+
+
+<?php
+/**
+ * The template for displaying all pages.
+ *
+ * This is the template that displays all pages by default.
+ * Please note that this is the WordPress construct of pages
+ * and that other 'pages' on your WordPress site will use a
+ * different template.
+ *
+ * @package Anima
+ */
+
+get_header(); ?>
+
+	<div id="container" class="<?php echo anima_get_layout_class(); ?>">
+
+		<main id="main" role="main" class="main">
+			<?php cryout_before_content_hook(); ?>
+
+
+		<!-- トップニュース -->
+			<div id="topnews" class="wakubox2"><!-- ニュース記事取得 -->
+				<?php query_posts('posts_per_page=1&order=desc&cat=5'); ?>
+				<?php while (have_posts()) : the_post(); ?>
+				<?php the_content(); ?>
+				<?php endwhile;wp_reset_query(); ?>
+			</div>
+		<br /><!-- トップニュースここまで -->
+
+			
+			<?php get_template_part( 'content/content', 'page' ); ?>
+
+			<?php cryout_after_content_hook(); ?>
+		</main><!-- #main -->
+
+		<?php anima_get_sidebar(); ?>
+
+	</div><!-- #container -->
+<?php get_footer();
+
